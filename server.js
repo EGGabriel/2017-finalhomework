4ec4ab0d875f228a04d4362d8955e7ab09b54f2d
EGGabriel <ps.eggabriel@gmail.com>
2017-11-30 02:22:31 -0200

    Add Connection with MYSQL

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = 3000
+const mysql = require('mysql')
+const session = require('express-session')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: "root",
+    password: "",
+    database: 'cpd'
+})
+
+const preference = {
+    connection
+}
 
 const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true, parameterLimit: 1000000  }))
 app.use(express.static('public'))
 
 app.set('views', path.join(__dirname, 'public'))
@@ -17,6 +30,10 @@ app.get('/', (request, response) => {
     response.render('index', data)
 })
 
-app.listen(PORT, () => { 
-    console.log('Server is running on port: ' + PORT)
+connection.connect(err => {
+    if (err) {
+        console.log('Error to connect to database ' + err)
+        return false
+    }
+    app.listen(PORT, () => console.log('Server running on port ' + PORT))
 })
